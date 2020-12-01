9ace7808315c62136baefdca636834ce52f531df
Kelvin Kipchirchir <kipchirchir1998@gmail.com>
2020-11-30 23:27:48 -0800

    include index page to login page,creating error

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,13 +1,19 @@
 @include('pages.index');
-<h1>user login</h1>
-<form method="post" action="/">
+<h3>user login</h3>
+<form method="post" action="/" class="col-sm-12 col-lg-12 col-md-12" >
 @csrf
-<label>Email:</label>
-<input type="email" name="email">
+<label>Email:</label><br>
+<input type="email" name="email"><br>
 <label>password:</label>
-<input type="password" name="password">
+<input type="password" name="password"><br>
 <button type="submit" class="btn btn-primary">Login</button>
-
-
 </form>
-</html>
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
