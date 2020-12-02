263dd8cb70bb8089ca573ee8753d73d06b922ef5
Kelvin Kipchirchir <kipchirchir1998@gmail.com>
2020-12-01 22:48:08 -0800

    extending sections to the parent

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -1,3 +1,9 @@
+@extends('pages.dashboard');
+@section('title','pagetitle');
+@section("sidebar")
+@parent
+<p>appended to the master parent</p>
+@endsection
 <html>
 @foreach($users as $user)
     {{$user->username}}
