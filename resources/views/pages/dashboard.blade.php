4ff3b886c2d6e2e348237de988d00507c59ab5d1
Kelvin Kipchirchir <kipchirchir1998@gmail.com>
2020-12-01 22:20:18 -0800

    styling email label and input

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="">
+	<title>daycare dashboard</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery.min.js')}}"></script>
 </head>
+<header>activities platform <a href='logout' style="float:right"; class="btn btn-primary";>sighout</a></header>
 <body>
-<h1>welcome;</h1>
+<div><div>
 </body>
 </html
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,8 +2,8 @@
 <h5>user login</h5>
 <form method="post" action="/" class="col-sm-12 col-lg-12 col-md-12" >
 @csrf
-<label>Email:</label><br>
-<input type="email" name="email"><br>
+<label for="email" class="col-md-4 control-label">Email:</label><br>
+<input type="email" id="email" class="form-control" name="email" require autofocus><br>
 <label>password:</label><br>
 <input type="password" name="password"><br><br>
 <button type="submit" class="btn btn-primary">Login</button>
